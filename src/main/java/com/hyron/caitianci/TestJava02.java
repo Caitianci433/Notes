e3898630034162fa86f037282fc7f9359d1bb37b
caitianci <caitianci@hyron.com>
2020-12-16 15:39:22 +0800

    test change

--- src/main/java/com/hyron/caitianci/TestJava02.java
+++ src/main/java/com/hyron/caitianci/TestJava02.java
@@ -5,14 +5,6 @@ package com.hyron.caitianci;
 
 public class TestJava02 {
     public static void main(String[] args) {
-        String str1 = "1";
-        String str2 = ""+1;
-        char ch1 = '1';
-        String str3 = new String("1");
-        System.out.println(ch1);
-        System.out.println(str1.hashCode());
-        System.out.println(str2.hashCode());
-        System.out.println(str3.hashCode());
-        System.out.println(str2.equals(str3));
+
     }
 }
