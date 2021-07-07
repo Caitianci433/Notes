bb7dc3d26f96573db1f526c9d0c374255d9bfed8
error <864502778@qq.com>
2021-07-07 21:57:13 +0800

    设计模式-建造者模式

---
+++ 设计模式练习/Builder/Builder.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    
+
+    public abstract class BuilderAbstract
+    {
+        public abstract void BuildPartA();
+
+        public abstract void BuildPartB();
+
+        public abstract void BuildPartC();
+
+        public abstract Product GetProduct();
+
+    }
+
+
+    public class Builder1 : BuilderAbstract
+    {
+        Product product = new Product();
+
+        public override void BuildPartA()
+        {
+            product.Add("PartA");
+        }
+
+        public override void BuildPartB()
+        {
+            product.Add("PartB");
+        }
+
+        public override void BuildPartC()
+        {
+            product.Add("PartC");
+        }
+
+        public override Product GetProduct()
+        {
+            return product;
+        }
+    }
+
+
+    public class Director1
+    {
+        public void Construct(BuilderAbstract builder)
+        {
+            builder.BuildPartA();
+            builder.BuildPartB();
+        }
+    }
+    public class Director2
+    {
+        public void Construct(BuilderAbstract builder)
+        {
+            builder.BuildPartA();
+            builder.BuildPartB();
+            builder.BuildPartC();
+        }
+    }
+
+
+
+
+
+    public class Product
+    {
+        private IList<string> parts = new List<string>();
+
+        public void Add(string part) 
+        {
+            parts.Add(part);
+        }
+
+        public string Run() 
+        {
+            return string.Join(',', parts);
+        
+        }
+    }
+}
---
+++ 设计模式练习/Builder/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Director1 director1 = new Director1();
+            Director2 director2 = new Director2();
+
+            Builder1 b1 = new Builder1();
+            Builder1 b2 = new Builder1();
+
+            director1.Construct(b1);
+            director2.Construct(b2);
+
+            Product p1 = b1.GetProduct();
+            Product p2 = b2.GetProduct();
+
+            Console.WriteLine(p1.Run());
+            Console.WriteLine(p2.Run());
+
+        }
+    }
+
+    
+
+}
