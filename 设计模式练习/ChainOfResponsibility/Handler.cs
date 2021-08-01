8405164a868ff7d0ece240210f13177475df8ed9
error <864502778@qq.com>
2021-08-01 19:12:12 +0800

    责任链模式

---
+++ 设计模式练习/ChainOfResponsibility/Handler.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibility
+{
+    public class Request 
+    { 
+        public int Level { get; set; }
+    
+    }
+    public class Response
+    {
+        public bool IsError { get; set; }
+
+        public string Errormessage { get; set; }
+    }
+
+    public abstract class IHttpHandler 
+    {
+        protected IHttpHandler next;
+
+        public void SetNEXT(IHttpHandler httpHandler) 
+        {
+            this.next = httpHandler;
+        }
+
+        public abstract Response Handler(Request request);
+
+    }
+
+    public class Filter1 : IHttpHandler
+    {
+
+        public override Response Handler(Request request)
+        {
+            if (request.Level > 1)
+            {
+                Console.WriteLine("Filer1 acess");
+                if (next!=null)
+                {
+                    return next.Handler(request);
+                }
+                else
+                {
+                    return new Response();
+                }     
+            }
+            else
+            {
+                return new Response();
+            }
+        }
+    }
+
+    public class Filter2 : IHttpHandler
+    {
+
+        public override Response Handler(Request request)
+        {
+            if (request.Level > 2)
+            {
+                Console.WriteLine("Filer2 acess");
+                if (next != null)
+                {
+                    return next.Handler(request);
+                }
+                else
+                {
+                    return new Response();
+                }
+            }
+            else
+            {
+                return new Response();
+            }
+        }
+    }
+
+    public class Filter3 : IHttpHandler
+    {
+
+        public override Response Handler(Request request)
+        {
+            if (request.Level > 3)
+            {
+                Console.WriteLine("Filer3 acess");
+                if (next != null)
+                {
+                    return next.Handler(request);
+                }
+                else
+                {
+                    return new Response();
+                }
+            }
+            else
+            {
+                return new Response();
+            }
+        }
+    }
+}
---
+++ 设计模式练习/ChainOfResponsibility/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ChainOfResponsibility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Filter1 = new Filter1();
+            var Filter2 = new Filter2();
+            var Filter3 = new Filter3();
+            Filter1.SetNEXT(Filter2);
+            Filter2.SetNEXT(Filter3);
+
+            var req1 = new Request() { Level = 1 };
+            var req2 = new Request() { Level = 2 };
+            var req3 = new Request() { Level = 3 };
+            var req4 = new Request() { Level = 4 };
+
+            Filter1.Handler(req1);
+            Filter1.Handler(req2);
+            Filter1.Handler(req3);
+            Filter1.Handler(req4);
+        }
+    }
+}
